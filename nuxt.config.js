3f48e6c571ec6e8e8c52dab01cdccbbf1873e6fa
cloudninefalling <shalygin.da7@gmail.com>
2024-03-19 20:09:47 +0300

    nuxt gh-pages config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/nuxt-cart/",
+    buildAssetsDir: "assets",
+  },
   devtools: { enabled: true },
   modules: ["nuxt-swiper"],
 });
